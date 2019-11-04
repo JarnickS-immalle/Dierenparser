0ec94ab37009538d83797c60bb8d1d000fb2b5c7
Sas Jarnick <jarnick.sas@immalle.eu>
2019-11-04 12:00:26 +0100

    Refactoren

--- dierenparser.py
+++ dierenparser.py
@@ -1,15 +1,14 @@
+from dataclasses import dataclass
 
+
+@dataclass
 class Dier:
 
-    def __init__(self):
-        self.naam = ""
-        self.soort = ""
-        self.aantalPoten = 0
-        self.kleur = ""
-        self.geluid = ""
-
-    def print(self):
-        print(f'Het dier heet [{self.naam}], is van soort [{self.soort}], heeft [{self.aantalpoten}] poten, is [{self.kleur}] en maakt dit geluid: [{self.geluid}]!')    
+    naam = ""
+    soort = ""
+    aantalPoten = 0
+    kleur = ""
+    geluid = ""
 
 
 def parse_line(line):
@@ -23,6 +22,7 @@ def parse_line(line):
     print(f'Parsed: [{naam}] [{soort}] [{aantalpoten}] [{kleur}] [{geluid}]')
     return d
 
+
 def parse_text(str):
     dieren = []
     for line in str.splitlines():
@@ -35,6 +35,6 @@ if __name__ == '__main__':
     dieren = []
     with open('dieren.txt', 'r') as f:
         dieren = parse_text(f.read())
-    
+
     for dier in dieren:
         dier.print()
